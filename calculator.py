4cf118d385371d273627f66160fb99658788d89e
fannysalmon <fysalmon@gmail.com>
2024-04-12 16:38:57 +0200

    Implémentation de la fonction de division dans calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def addition(self,a, b):
+        result = a + b
+        return result
+    
+    def substraction(self,a, b):
+        result = a - b
+        return result
+    
+    def division(self, a, b):
+        
+        if b != 0:
+            return a / b
+        else:
+            
+            print("Erreur : Division par zéro !")
+            return None
