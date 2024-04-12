df12dde3df83c920feaa1cb6dae339dfc924ebbe
fannysalmon <fysalmon@gmail.com>
2024-04-12 16:28:51 +0200

    Implémentation de la fonction d'addition dans calculator.py

--- calculator.py
+++ calculator.py
@@ -2,15 +2,3 @@ class Calculator:
     def addition(self, a, b):
         result = a + b
         return result
-
-# Importez la classe Calculator depuis votre fichier calculator.py
-from calculator import Calculator
-
-# Créez une instance de la classe Calculator
-calc = Calculator()
-
-# Appelez la méthode addition() sur l'instance avec des valeurs d'exemple pour a et b
-a = 10
-b = 5
-result = calc.addition(a, b)
-print("Résultat de l'addition :", result)
